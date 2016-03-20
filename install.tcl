68c1fef1af7dcd84af51ae006cc00c6d4e07b897
Wouter Snels <nospam@ofloo.net>
2016-03-20 18:30:11 +0100

    fixed install, when directory doesn't exist

--- install.tcl
+++ install.tcl
@@ -32,13 +32,13 @@ if {[file exists ${tcl_pkgPath}/ip2c] && [file isdirectory ${tcl_pkgPath}/ip2c]}
     puts stdout "Before you install ip2c you need to deinstall previous installations."
     return 2
   } else {
-    file mkdir ${tcl_pkgPath}/ip2c
     file copy -force [pwd]/ip2c.tcl ${tcl_pkgPath}/ip2c
     file copy -force [pwd]/pkgIndex.tcl ${tcl_pkgPath}/ip2c
     puts stdout "Installed ip2c package."
     return 0
   }
 } else {
+  file mkdir ${tcl_pkgPath}/ip2c
   file copy -force [pwd]/ip2c.tcl ${tcl_pkgPath}/ip2c
   file copy -force [pwd]/pkgIndex.tcl ${tcl_pkgPath}/ip2c
   puts stdout "Installed ip2c package."
