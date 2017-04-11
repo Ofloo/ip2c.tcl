9f74f73bed387897121aaf9093eff75b2ff7108a
Wouter Snels <nospam@ofloo.net>
2017-04-11 16:02:10 +0200

    relisenced to bsd lisence, changed to https

--- ip2c.tcl
+++ ip2c.tcl
@@ -7,19 +7,33 @@
 #  GIT: https://github.com/Ofloo/ip2c.tcl
 #  Email: support[at]ofloo.net
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
+#  Copyright (c) 2017, Wouter Snels
+#  All rights reserved.
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU General Public License for more details.
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#  3. All advertising materials mentioning features or use of this software
+#     must display the following acknowledgement:
+#     This product includes software developed by the <organization>.
+#  4. Neither the name of the <organization> nor the
+#     names of its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written permission.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#  THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
+#  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 ################################################################################
 
@@ -27,9 +41,11 @@ namespace eval ip2c {
 
   package require http
   package require ip
+  package require tls
+  package require Tcl 8.4
 
   variable api "api.ip2c.info"
-  variable version 1.2.1
+  variable version 1.3.0
 
   variable registry
   variable assigned
@@ -61,9 +77,10 @@ namespace eval ip2c {
     if {![info exists ip]} {set ip {}}
     set out "0"
     if {[ip::is ipv4 $ip] || [ip::is ipv6 $ip] || ($ip == "")} {
-      set url "http://${api}/csv/${ip}"
+      http::register https 443 tls::socket
+      set url "https://${api}/csv/${ip}"
       while {1} {
-        if {![catch {http::geturl $url} tok]} {
+        if {![catch {http::geturl $url -timeout 30000} tok]} {
           switch -glob -- [http::ncode $tok] {
             30[1237] {
               upvar #0 $tok state
@@ -90,6 +107,7 @@ namespace eval ip2c {
 
       set dat [http::data $tok]
       http::cleanup $tok
+      http::unregister https
       cleanup
       foreach {-> ip a02 a03 coun reg time} [regexp -all -nocase -inline {"(.*?)","([a-z]{2})","([a-z]{3})","(.*?)","([a-z]+)","([0-9]+)"} $dat] {
         array set registry [list $out $reg]
